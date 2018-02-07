a8c1e5c800bc61812d51cbf942cf0cd56cd68728
Adonai Diofanes <adonaibm@gmail.com>
2018-02-07 14:47:32 -0200

    Estrutura do banco de dados

--- banco.sql
+++ banco.sql
@@ -1,1 +1,38 @@
-CREATE TABLE pagamentos ( id int(11) NOT NULL AUTO_INCREMENT, forma_de_pagamento varchar(255) NOT NULL, valor decimal(10,2) NOT NULL, moeda varchar(3) NOT NULL, status varchar(255) NOT NULL, data DATE, descricao text, PRIMARY KEY (id) );
+/*
+SQLyog Ultimate v10.00 Beta1
+MySQL - 5.7.14 : Database - sys_pagamento
+*********************************************************************
+*/
+
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`sys_pagamento` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `sys_pagamento`;
+
+/*Table structure for table `pagamentos` */
+
+DROP TABLE IF EXISTS `pagamentos`;
+
+CREATE TABLE `pagamentos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `forma_de_pagamento` varchar(255) NOT NULL,
+  `valor` decimal(10,2) NOT NULL,
+  `moeda` varchar(3) NOT NULL,
+  `status` varchar(255) NOT NULL,
+  `data` date DEFAULT NULL,
+  `descricao` text,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
